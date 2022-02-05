fd5f3a9e158e22cf85e2f3d552a3a4ad937d2f43
Arturito <arturs.jaloveckis@outlook.lv>
2022-02-05 15:10:02 +0200

    Project

---
+++ ProjectMxedMessage.js
@@ -0,0 +1,51 @@
+
+
+
+
+const firstWordArray = ['pipes', 'crime', 'murderers', 'giants', 'airplanes', 'nurses', 'gypsies', 'spas', 'money', 'lighters', 'motherhood', 'amusement', 'parks', 
+'motorcycles', 'golf', 'masonry', 'firemen', 'native', 'bohemians', 'wrestling', 'astronomy', 'pork', 'camels', 'pigeons', 'zebras', 'radio', 'printing',
+'slavery', 'parachuting', 'x-rays', 'horses', 'railroads', 'mining', 'games', 'fencing', 'sugar', 'hockey', 'books', 'prisons', 'marionettes', 'shipwrecks', 'dolphins', 
+'dentistry', 'execution', 'laundry', 'rhinoceros', 'agriculture', 'cactus', 'pipe', 'gymnastics', 'clowns'];
+
+const verbArray = ['pronounce', 'doubt', 'disappear', 'tax', 'note', 'rent', 'enable', 'question', 'constitute', 'blame', 'rain', 'centre', 'resign', 'feature', 'mutter', 'reveal', 
+'pass', 'determine', 'flood', 'consist', 'tempt', 'hook', 'compare', 'devote', 'expect', 'dispose', 'lose', 'come', 'split', 'desire', 'burn', 'accumulate', 'sail', 'draft', 'visit', 
+'direct', 'predict', 'mark', 'have', 'strike', 'recover', 'defend', 'accommodate', 'offer', 'ban', 'install', 'listen', 'forbid', 'spin'];
+
+const adverbArray = ['meaningfully', 'clearly', 'unimpressively', 'willfully', 'viciously', 'wetly', 'colorfully', 'happily', 'literally', 'elegantly', 'zestily', 'heavily',
+'strictly', 'sometimes', 'faithfully', 'upward', 'rather', 'positively', 'inwardly', 'wisely', 'officially', 'almost', 'freely', 'strongly', 'joshingly', 'mechanically', 'busily',
+'hastily', 'mortally', 'effectively', 'jaggedly', 'vainly', 'obediently', 'politely', 'merrily', 'slowly', 'neatly', 'loosely', 'kookily', 'eagerly', 'healthily', 'boldly', 'bravely',
+'somewhat', 'mockingly', 'terrifically', 'thoroughly', 'interestingly', 'early', 'briskly'];
+
+const subjectArray = ['truck', 'flock', 'swing', 'bread', 'patch', 'sugar', 'unit', 'maid', 'school', 'friends', 'grip', 'fold', 'wound', 'vessel', 'tub', 'current', 'division', 'seat',
+'look', 'test', 'measure', 'arch', 'faucet', 'skate', 'color', 'vase', 'winter', 'hope', 'rock', 'stranger', 'girl', 'shape', 'pain', 'coal', 'wheel', 'arm', 'library', 'action', 'pencil',
+'chair', 'pear', 'example', 'pets', 'riddle', 'list', 'sack', 'leather', 'team', 'church', 'beds'];
+
+const unionArr = ['and', 'or', 'within', 'with', 'for', 'of' ];
+
+const randomNumbeWord = (array) => {
+
+    return Math.floor(Math.random() * array.length ) 
+
+}
+
+
+const returnString = () => {
+
+    let upperCase = subjectArray[randomNumbeWord(subjectArray)];
+
+    upperCase = upperCase[0].toUpperCase()  + upperCase.slice(1, );
+
+
+
+    return `${upperCase} ${verbArray[randomNumbeWord(verbArray)]} ${firstWordArray[randomNumbeWord(firstWordArray)]} ${unionArr[randomNumbeWord(unionArr)]} ${verbArray[randomNumbeWord(verbArray)]} ${adverbArray[randomNumbeWord(adverbArray)]} ${firstWordArray[randomNumbeWord(firstWordArray)]}.`;
+
+
+}
+
+
+console.log(returnString());
+console.log(returnString());
+console.log(returnString());
+console.log(returnString());
+console.log(returnString());
+console.log(returnString());
---
+++ README.md
@@ -0,0 +1,29 @@
+# Mexed Mesaage (Detective Book Name)
+
+## Project property's
+
++ Javascript Based
++ Run with node.js
+
+## Project discription 
+
+    How it should work? Programm automaticly generate new detective book name. I hope someone would like to buy it. 
+
+
+### Program structure
+
+
+1. Code inside have an array of diifirent words 
+2. Function randomly generate random number if array index. 
+3. Other function return string from array, that will our name.  
+
+### Code structure
+
+1. We have 5 array. 4 of them each contain 50 word depent on sentance structure: subjects, verbs, adverbs. And unions array.
+2. Function generate random index based on array length. 
+3. Final function create first letter of word capital and compire in word with captial first letter using .tuUpperCase and .slice methods
+4. Then pull all together using template string. 
+
+### Conclusion
+
+As result we can get a detective book name, a little bit non-sense, mistery or stupid. 
